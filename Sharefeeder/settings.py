1f0554ac4975697c780fa7f712ae91718d6d71b2
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2018-10-03 20:50:39 -0500

    Configure database connection for MySQL

--- Sharefeeder/settings.py
+++ Sharefeeder/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Sharefeeder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'init_command': 'SET default_storage_engine=INNODB',
+            'read_default_file': 'database.cnf',
+        }
     }
 }
 
