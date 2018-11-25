dfe74a497dab8606632bb09c8ebda3430af61c1a
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2018-11-24 20:23:52 -0600

    Implement signin and signup forms UI elements

--- feeder/static/feeder/js/main.js
+++ feeder/static/feeder/js/main.js
@@ -1,10 +1,69 @@
+Vue.use(VeeValidate);
+
 new Vue({
     el: '#app',
-    data() {
+    $_veeValidate: {
+        validator: 'new'
+    },
+
+    data: () => ({
+        signupName: "",
+        signupEmail: "",
+        signupPassword: "",
+        signinEmail: "",
+        signinPassword: "",
+        selectFeeder: null,
+        show1: false,
+        dictionary: {
+            attributes: {
+                email: 'E-mail Address'
+                // custom attributes
+            },
+            custom: {
+                name: {
+                    required: () => 'Nombre no puede estar vacio',
+                    max: "El nombre no puede ser mayor a 85 caracteres",
+                    // custom messages
+                }
+            },
+            selectFeeder: {
+                required: 'Se requiere seleccionar una alimentadora'
+            }
+        }
+    }),
+
+    mounted () {
+        this.$validator.localize('en', this.dictionary)
+    },
+
+    methods: {
+        submit() {
+            this.$validator.validateAll()
+        },
+        clear() {
+            this.name = ""
+            this.email = ""
+            this.password = ""
+            this.selectFeeder = null
+            this.$validator.reset()
+        }
+    },
+
+});
+
+
+/*
+{
+        name: "",
+        emmail: "",
+        password: "",
         return {
             loading3: false,
             selectFeeder: 0,
             items: [],
+
         }
     }
 });
+*/
+
